a89fe26f62c57af227592b0f48f1723b63c8cde9
EdwardSnyder-NOAA <Edward.Snyder@noaa.gov>
2026-01-21 20:04:35 +0000

    switch to externalized spack-stack container

---
+++ modulefiles/ufs_noaacloud.gnu.lua
@@ -0,0 +1,21 @@
+help([[
+loads UFS Model prerequisites for NOAA Parallelworks/GNU
+]])
+
+prepend_path("MODULEPATH", "/contrib/Edward.Snyder/gnu-cont/auto-built/w-cont-scripts/modulefiles/spack-stack-1.9.2/Core")
+
+-- path to gnu and openmpi from the container
+prepend_path("MODULEPATH", "/contrib/Edward.Snyder/gnu-cont/auto-built/w-cont-scripts/modulefiles/spack-stack-1.9.2/container-software/modulefiles")
+
+-- load gnu and openmpi from the container
+load("gnu/13.3.1")
+load("openmpi/4.1.6")
+
+load("stack-gcc")
+load("stack-openmpi")
+
+load("ufs_common")
+
+setenv("CMAKE_Platform", "noaacloud.gnu")
+
+whatis("Description: UFS build environment")
